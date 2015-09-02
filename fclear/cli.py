8d11501128ab538b1ce48311c3e1e06f1dd78361
Parkayun <iamparkayun@gmail.com>
2015-09-02 23:27:53 +0900

    Improve cli using argparser

--- fclear/cli.py
+++ fclear/cli.py
@@ -1,14 +1,20 @@
 # -*- coding:utf-8 -*-
+from argparse import ArgumentParser, RawTextHelpFormatter
 from sys import argv
 
 from .cleaner import Cleaner
 
 
 def run():
-    try:
-        language = argv[1]
-    except IndexError:
-        language = ''
+    parser = ArgumentParser(description='Clear compiled files.',
+                            formatter_class=RawTextHelpFormatter)
+    parser.add_argument('language', type=str, help='\n'.join((
+        'python ------ Remove pyc, __pycache__',
+        'py ---------- Remove pyc, __pycache__',
+        'java -------- Remove class',
+        'vim --------- Remove swp',
+        'vi ---------- Remove swp',
+    )))
 
-    cleaner = Cleaner(language)
+    cleaner = Cleaner(parser.parse_args().language)
     cleaner.run()
